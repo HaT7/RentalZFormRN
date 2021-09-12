3c6da8d222b7eb964471258f0d8f19be80e0bb5f
trinm31 <trinmgcd19833@fpt.edu.vn>
2021-09-12 20:21:32 +0700

    Init project

---
+++ src/features/InformationForm/screens/informationForm.screen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+
+function InformationFormScreen() {
+  return (
+    <SafeArea>
+      <Text>This is information screen</Text>
+    </SafeArea>
+  );
+}
+
+export default InformationFormScreen;
